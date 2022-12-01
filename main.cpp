ff069ff0066b7ab9ff0aa23ccab79d9514d694eb
Ludwig <kmouren@qq.com>
2022-12-01 23:28:27 +0800

    git commit -m '已有功能：初始化、添加单个节点、打印班级、打印年级、全部打印'

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 #define MAX_GRADE 4
 #define MAX_CLASS 10
-
+//课程与成绩
 typedef struct LESSON{
     int math;
     int eng;
     int phy;
 }Lesson;
-
+//学生信息--链表
 typedef struct STU{
     int Grade_Name;
     int Class_Name;
@@ -18,12 +18,12 @@ typedef struct STU{
     Lesson*Mark;
     STU*next;
 }STU;
-
+//班级--顺序表
 typedef struct CLASS{
     int Class_Num;
     STU*FirsrStu;
 }CLASS;
-
+//年级--顺序表
 typedef struct GRADE{
     int Grade_Num;
     CLASS Class[MAX_CLASS];
