f97035dccc9a8c1afd1c4cfd20d6f64343d9f06b
Ludwig <kmouren@qq.com>
2022-12-01 19:11:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -52,10 +52,12 @@ int InitList(LIST &L,int GNum,int CNum){
             L.Grade[i].Class[j].FirsrStu=p;
         }
     }
-    for(int i=0;i<CNum;i++){
-        if(i=CNum){
-            L.Grade[0].Class[i].FirsrStu->next=NULL;
+    for(int j=0;j<GNum;j++){
+        for(int i=0;i<CNum;i++){
+            if(i=CNum){
+                L.Grade[j].Class[i].FirsrStu->next=NULL;
+            }
+            L.Grade[j].Class[i].FirsrStu->next=L.Grade[j].Class[i+1].FirsrStu;
         }
-        L.Grade[0].Class[i].FirsrStu->next=L.Grade[0].Class[i+1].FirsrStu;
     }
 }
